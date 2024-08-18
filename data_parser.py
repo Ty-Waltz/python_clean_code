2ca6c4f2cad5ece00072ae8776bff534aea2db54
Ty-Waltz <ty.waltz@yahoo.com>
2024-08-18 16:44:16 -0400

    created a cleaner version of the 'refactoring_weather' code

---
+++ data_parser.py
@@ -0,0 +1,10 @@
+class DataParser:
+    def parse_weather_data(data):
+    
+        if not data:
+            return "Weather data not available"
+        city = data["city"]
+        temperature = data["temperature"]
+        condition = data["condition"]
+        humidity = data["humidity"]
+        return f"Weather in {city}: {temperature} degrees, {condition}, Humidity: {humidity}%"
---
+++ main.py
@@ -0,0 +1,8 @@
+from user_interface import UserInterface
+
+def main():
+    ui = UserInterface()
+    ui.start()
+
+if __name__ == "__main__":
+    main()
